2894e9773cdc3f56a02f553a76b310c6f58a4515
Alex Chubaty <achubaty@for-cast.ca>
2023-10-05 11:39:12 -0600

    skip tests without geodata + googledrive

--- tests/testthat/test-makeClimateDEM.R
+++ tests/testthat/test-makeClimateDEM.R
@@ -1,6 +1,8 @@
 test_that("makeClimateDEM works", {
   skip_on_cran()
   skip_if_not(identical(tolower(.Platform$OS.type), "windows"))
+  skip_if_not_installed("geodata")
+  skip_if_not_installed("googledrive")
 
   dPath <- file.path(tempdir(), "test_makeClimateDEM")
 
