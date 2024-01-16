898225524c0e4f0afd4219d5b27edca127ca4be8
Alex Chubaty <achubaty@for-cast.ca>
2024-01-15 20:36:27 -0700

    allow historic and future normals as 'type' in ClimateNA_path()

--- R/ClimateNA.R
+++ R/ClimateNA.R
@@ -36,7 +36,7 @@ tileID <- function(file) {
 #'
 #' @param dataPath character. path to top-level directory for reading/writing ClimateNA data.
 #' @param tile integer. tile id (e.g., the output of `tileID()`).
-#' @param type character. one of 'normals', 'historic', or 'future'.
+#' @param type character. one of 'historic', 'future', 'historic_normals', or 'future_normals'.
 #' @param msy character. one of 'MSY', 'M', or 'Y', corresponding to all, monthly, or yearly data.
 #' @param gcm character. name of GCM available in ClimateNA.
 #' @param ssp integer. one of the available SSPs in ClimateNA (e.g., `370` or `585`)
@@ -45,15 +45,15 @@ tileID <- function(file) {
 #'
 #' @export
 ClimateNA_path <- function(dataPath, tile = NULL, type = NULL, msy = NULL, gcm = NULL, ssp = NULL) {
-  stopifnot(type %in% c("normals", "historic", "future"))
+  stopifnot(type %in% c("historic", "historic_normals", "future", "future_normals"))
 
+  MSY <-  switch(msy, MSY = "all", M = "monthly", Y = "yearly")
   switch(
     type,
-    normals = file.path(dataPath, "historic", "normals", tile),
-    historic = file.path(dataPath, "historic",
-                         switch(msy, MSY = "all", M = "monthly", Y = "yearly"), tile),
-    future = file.path(dataPath, "future", paste0(gcm, "_ssp", ssp),
-                       switch(msy, MSY = "all", M = "monthly", Y = "yearly"), tile)
+    historic = file.path(dataPath, "historic", MSY, tile),
+    historic_normals = file.path(dataPath, "historic", "normals", MSY, tile),
+    future = file.path(dataPath, "future", paste0(gcm, "_ssp", ssp), MSY, tile),
+    future_normals = file.path(dataPath, "future", "normals", MSY, tile)
   ) |>
     normalizePath(mustWork = FALSE) |>
     (function(x) {
