688bdbe863692e2e8bf8fd1632b2998fcdfdd06b
Alex Chubaty <achubaty@for-cast.ca>
2024-02-21 20:36:39 -0700

    add note re: cluster setup 'sequential' vs 'parallel'

--- man-roxygen/cl.R
+++ man-roxygen/cl.R
@@ -2,4 +2,6 @@
 #'           `length(climVar)` number of CPU cores for parallel computation.
 #'           Users can pass their own `cl` object or specify option
 #'           `parallelly.availableCores.fallback` to reduce the number of cores used.
+#'           Users may also need to specify setting up the cluster using 'sequential',
+#'           rather than the default 'parallel', via `parallelly.makeNodePSOCK.setup_strategy`.
 #'           See `?parallelly.options`.
