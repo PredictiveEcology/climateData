90d3dca401a404a3f02f94b2cf57adb838ee293f
Eliot McIntire <eliotmcintire@gmail.com>
2025-09-15 14:47:38 -0700

    getFromNamespace: missing reproducible:::

---
+++ R/imports.R
@@ -0,0 +1,6 @@
+#' non-exported objects and functions from other packages
+#'
+#' @importFrom utils getFromNamespace
+#' @keywords internal
+#' @rdname imports
+assessGoogle <- utils::getFromNamespace("assessGoogle", "reproducible")
